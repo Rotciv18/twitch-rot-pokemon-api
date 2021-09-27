a5f2643d2008ac31184638137fde3f7ca851bda9
Victor Batista <v.rotciv@hotmail.com>
2021-09-26 23:25:16 -0300

    updating alertPokemonQueue to connect to another redis db

--- src/queues/alertPokemonQueue.js
+++ src/queues/alertPokemonQueue.js
@@ -1,3 +1,9 @@
 import Queue from 'bull';
 
-export default new Queue('AlertPokemonQueue');
+export default new Queue('AlertPokemonQueue', {
+  redis: {
+    password: process.env.REDIS_PASSWORD,
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+});
